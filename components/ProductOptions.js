3afef455b40465fb4e20d66c7f9b0b3460466b48
Rocky Nguyen <rockynhatnguyen@gmail.com>
2022-04-26 21:37:48 -0700

    Added product form UI

---
+++ components/ProductOptions.js
@@ -0,0 +1,34 @@
+
+
+export default function ProductOptions({ name, values, selectedOptions, setOptions }) {
+  return (
+    <fieldset>
+        <legend className="text-xl font-semibold">{name}</legend>
+        <div className="flex-wrap inline-flex items-center">
+            {
+                values.map(value => {
+                    const id = `option-${name}-${value}`
+                    const checked = selectedOptions[name] === value
+
+                    return (
+                        <label key={id} htmlFor={id}>
+                            <input 
+                                className="sr-only"
+                                type="radio" 
+                                id={id}
+                                name={`option-${name}`}
+                                value={value}
+                                checked={checked}
+                                onChange={() => setOptions(name, value)}
+                            />
+                            <div className={`p-2 my-3 mr-3 text-lg rounded-full block cursor-pointer ${checked ? "text-white bg-gray-900" : "text-gray-900 bg-gray-200"}`}>
+                                <span className="px-2">{value}</span>
+                            </div>
+                        </label>
+                    )
+                })
+            }
+        </div>
+    </fieldset>
+  )
+}
--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,9 +1,15 @@
-
+import Image from 'next/image'
+import ProductForm from './ProductForm'
 
 export default function ProductPageContent({ product }) {
   return (
-    <div>
-        {product.title}
+    <div className="max-w-6xl w-11/12 mx-auto flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8">
+        <div className="max-w-md w-full border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
+          <div className="w-full h-96 relative">
+            <Image src={product.images.edges[0].node.url} alt={product.images.edges[0].node.altText} layout="fill" objectFit="cover" />
+          </div>
+        </div>
+        <ProductForm product={product} />
     </div>
   )
 }
